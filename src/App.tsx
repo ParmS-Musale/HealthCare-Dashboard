7e1027b97971c7ea33fa260e4cefd4cb795fb902
Parmeshwar Musale <152637541+ParmS-Musale@users.noreply.github.com>
2025-05-25 18:40:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
+        // Dashboard
         
         <main className="flex-1 overflow-y-auto">
           <Dashboard />
